d9d6972a6817b985c84c44841f298c649662e175
Chitranshu_V_varughese <varughesechitranshu@gmail.com>
2026-08-15 20:16:54 +0530

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hash_map = {}
+        for i, num in enumerate(nums):
+            if num in hash_map:
+                return True
+            else:
+                hash_map[num] = i
+        return False        
